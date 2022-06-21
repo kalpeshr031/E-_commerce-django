a5105f68988f4cd56822463b3ba147c81c68f9d3
kali <kalpeshr031@gmail.com>
2022-06-21 07:58:59 -0400

    Fixed migrate error

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,17 @@
+from email.policy import default
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
 
 
-class product(models.Model):
+class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
     desc = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    sub_category= models.CharField(max_length=50)
     pub_date = models.DateField()
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="shop/images",default="")
+    
